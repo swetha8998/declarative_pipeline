d3ac46d707b7659e14439b4d1e86728e28065c75
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 15:16:27 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,19 +2,27 @@ pipeline{
 agent any 
 stages{
 stage('git'){
+  step{
 git branch: 'branch1', url: 'https://github.com/swetha8998/declarative_pipeline.git'
+  }
 }
 stage('Build'){
+  step{
 sh 'echo "in building stage"'
+  }
 }
 stage('Approval'){
+  step{
 sh 'echo "in approval stage" '
   timeout(time: 15, unit: "MINUTES") {
     input message: 'Do you want to approve the deploy in production?', ok: 'Yes'
 }
+  }
 }
 stage('Deploy'){
+  step{
 sh 'echo "in deployment stage" '
  }
+}
  }
  }
