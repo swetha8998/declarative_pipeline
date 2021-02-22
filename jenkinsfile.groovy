71bcfed11829339364bfd52ce579fdee4d4ddc7f
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 15:09:43 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,20 @@
+pipleline{
+agent any 
+stages{
+stage('git'){
+git branch: 'branch1', url: 'https://github.com/swetha8998/scripted_pipeline.git'
+}
+stage('Build'){
+sh 'echo "in building stage"'
+}
+stage('Approval'){
+sh 'echo "in approval stage" '
+  timeout(time: 15, unit: "MINUTES") {
+    input message: 'Do you want to approve the deploy in production?', ok: 'Yes'
+}
+}
+stage('Deploy'){
+sh 'echo "in deployment stage" '
+ }
+ }
+ }
