a2e0c1461f0b00d4c6ee9e0c50291c10bd0dee3b
SWETHA S <43613703+swetha8998@users.noreply.github.com>
2021-02-22 15:14:28 +0530

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -2,7 +2,7 @@ pipeline{
 agent any 
 stages{
 stage('git'){
-git branch: 'branch1', url: 'https://github.com/swetha8998/scripted_pipeline.git'
+git branch: 'branch1', url: 'https://github.com/swetha8998/declarative_pipeline.git'
 }
 stage('Build'){
 sh 'echo "in building stage"'
